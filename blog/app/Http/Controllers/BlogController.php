bc25e179d0d73839097739b586b729cf2e79da18
iqbalcakep <iqbalacakep.com@gmail.com>
2019-07-16 12:07:52 +0700

    database dan query builder

--- blog/app/Http/Controllers/BlogController.php
+++ blog/app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
@@ -11,7 +12,12 @@ class BlogController extends Controller
     }
 
     function show($id){
-        $users=["iqbalcakep","iqbalganteng","iqbalprogrammer"];
+        // DB::table('tb_user')->insert([
+        //     "username"=>"siapa aja",
+        //     "password"=> "123"
+        // ]);
+        $users=DB::table('tb_user')->get()->where("id",$id);
+
        return View("blog/Single",["id" => $id,"users"=>$users]);
     }
 }
--- blog/resources/views/blog/Single.blade.php
+++ blog/resources/views/blog/Single.blade.php
@@ -5,8 +5,9 @@
 @section('konten')
     <h1>Selamat Datang {{$id}}</h1>
     @foreach ($users as $v)
-        <li>{{$v}}</li>        
+        <li>{{$v->username}}</li>        
     @endforeach
+
 
     @if (count($users)>5)
         User Lebih Dari 5
